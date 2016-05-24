fed1f0cd05c999b5b7d1a9a966b040acd5c46758
Florian Kistner <mail@fkistner.com>
2016-05-25 01:02:54 +0200

    Initial Commit

---
+++ OrgChartGen/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  OrgChartGen
+//
+//  Created by Florian Kistner on 25/05/16.
+//  Copyright © 2016 Florian Kistner. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
